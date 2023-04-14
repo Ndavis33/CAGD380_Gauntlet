f7b4f629ade00e7a12f1698755b9f934083aa2a0
GrantHS <6nohx1@gmail.com>
2023-04-14 16:19:39 -0700

    Multi-target tracing
    The enemy can now chase the closest of total targets and deals damage to them via coroutine. (Test damage)

--- 380_Gauntlet/Assets/Scripts/Grants Scripts/EnemyMovement.cs
+++ 380_Gauntlet/Assets/Scripts/Grants Scripts/EnemyMovement.cs
@@ -5,15 +5,97 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     //private GameObject _warrior, _valkyrie, _wizard, _elf;
-    private GameObject player;
+    private GameObject closestTarget;
+    private Vector3 targetPos;
+    private bool attackingPlayer;
+    protected float speed;
+    protected int damage;
+    private List<TestMovement> _targets = new List<TestMovement>();
 
     private void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").gameObject;
+        speed = 3f;
+        damage = 25;
+
+        foreach (TestMovement target in GameObject.FindObjectsOfType<TestMovement>())
+        {
+            _targets.Add(target);
+            Debug.Log("found target: " + target.name);
+        }
     }
 
     private void FixedUpdate()
     {
-        this.transform.Translate(player.transform.position);
+        float minDistanceFromTarget = 420.69f;
+        foreach (TestMovement target in _targets)
+        {
+            if (Vector3.Distance(this.transform.position, target.transform.position) < minDistanceFromTarget)
+            {
+                if (target.gameObject.activeInHierarchy)
+                {
+                    minDistanceFromTarget = Vector3.Distance(this.transform.position, target.transform.position);
+                    closestTarget = target.gameObject;
+
+                }
+                
+            }
+                
+        }
+
+        Debug.Log("Closest Target: " + closestTarget.name);
+        targetPos = closestTarget.transform.position;
+        this.transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.fixedDeltaTime);       
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Player"))
+        {
+            attackingPlayer = true;
+            StartCoroutine(DamagePlayer(collision.collider.gameObject));
+         
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Player"))
+        {
+            attackingPlayer = false;
+        }
+    }
+
+    private IEnumerator DamagePlayer(GameObject player)
+    {
+        //example player stats for testing
+        damage = 25;
+        float playerHP = 100;
+        float tempSpeed;
+        if (attackingPlayer)
+        { 
+            while(playerHP > 0)
+            {
+                Debug.Log("Attacking player");
+                playerHP -= damage;
+
+                if(playerHP <= 0)
+                {
+                    player.SetActive(false);
+                    Debug.Log("Target eliminated");
+                    break;
+                }
+                tempSpeed = speed;
+                speed = 0;
+                yield return new WaitForSeconds(1);
+                speed = tempSpeed;
+
+                if (!attackingPlayer)
+                    break;
+
+            }
+            
+        }
+        
+        
+
     }
 }
