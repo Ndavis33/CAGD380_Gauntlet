cc0df48fbbe51de04aaa0b18bece3978d79fa1aa
GrantHS <6nohx1@gmail.com>
2023-05-04 13:57:24 -0700

    Started Generator

---
+++ 380_Gauntlet/Assets/Scripts/Grants Scripts/EnemyGenerator.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public EnemyScriptableObject enemySO;
+
+    private Vector3 _spawnPos;
+    private Vector3 _spawnOffset = new Vector3(0, 0.625f);
+
+    private float _spawnRate = 3f;
+
+    private void Awake()
+    {
+        this.GetComponent<Renderer>().material.color = enemySO.enemyColor;
+        _spawnPos = gameObject.transform.position;
+        _spawnPos += _spawnOffset;
+
+    }
+
+    private void Start()
+    {
+        StartCoroutine(GenerateSpawns(_spawnRate));
+    }
+
+    private GameObject SpawnEnemy()
+    {
+        GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        obj.transform.position = _spawnPos;
+        obj.AddComponent<EnemyMovement>();
+        obj.GetComponent<EnemyMovement>().enemySO = enemySO;
+        return obj;
+    }
+
+    private IEnumerator GenerateSpawns(float spawnRate)
+    {
+        while (true)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
+}
